244e9a8b9d049060557ca9ef62497348a4ec02a1
Vikasthangavel <152574274+Vikasthangavel@users.noreply.github.com>
2024-10-13 02:15:14 +0000

    4fuctionadded

--- expensetracker.py
+++ expensetracker.py
@@ -1,4 +1,23 @@
 import time
+addexpense=[]
+addincome=[]
+class expense:
+    def addexpense():
+        expense=int(input("Enter the your Expense:"))
+        addexpense.append(expense)
+        total=sum(addexpense)
+        print(total)
+    def addincome():
+        income=int(input("Enter your income:"))
+        addincome.append(income)
+        totalin=sum(addincome)
+    def balance():
+        balance=sum(addincome)-sum(addexpense)
+        print("The remaining amount:",balance)
+    def viewexpense():
+        for i in range(len(addexpense)):
+            print(addexpense[i])
+        
 #toread choice
 while(True):
     print("------------------------------------------------------------------------------------------------")
@@ -7,15 +26,16 @@ while(True):
     choice=int(input("Enter your choice:"))
     if(choice==1): 
         print("calling")
-        #call add expense()
+        expense.addexpense()
     elif(choice==2):
          print("calling")
-        #call add income()
+         expense.addincome()
     elif(choice==3):
         print("calling")
-        #call view Balance()
+        expense.balance()
     elif(choice==4):
          print("calling")
+         expense.viewexpense()
         #call view Expense()
     elif(choice==5):
         print("calling")
